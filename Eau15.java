507a53c125a4187c5432d860f6ef474e2b421c74
krokib <bblondeau@netcourrier.com>
2025-02-03 14:31:41 +0100

    Ajout de Eau15.java

---
+++ Eau15.java
@@ -0,0 +1,19 @@
+/*
+ * Epreuve eau - Eau : ok
+ * 
+ * Créez un programme qui célèbre votre victoire.
+ * 
+ * Exemples d’utilisation :
+ * $> ruby exo.rb
+ * J’ai terminé l’Épreuve de l’Eau et c’était [].
+ * $>
+ * 
+ * Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+ */
+
+ public class Eau15 {
+ 
+    public static void main(String[] args) {
+        System.out.println("J’ai terminé l’Épreuve de l’Eau et c’était l'éclate totale !");
+    }
+ }
